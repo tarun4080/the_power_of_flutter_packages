514526cb092ae9b7588584624dd9dc371c398e7f
Tarun Kumar <tarun@liberintechnologies.com>
2020-08-25 12:35:12 +0530

    Added multi_selected_flutter package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:thepoweofflutterpackage/flutter_packages/multi_select_flutter.dart';
 import 'package:thepoweofflutterpackage/flutter_packages/package_info.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'The power of Flutter package',
 
-      home: FlutterAppInfo(),
+      home: MultiSelectFlutterDemo(),
     );
   }
 }
